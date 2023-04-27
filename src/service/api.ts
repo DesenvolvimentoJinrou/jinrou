581f136b9bff86c1ea5b3779daf4f32124a34d6b
vinereis <viniciusreis1001@gmail.com>
2023-04-26 22:41:03 -0300

    Alterando o Servidor

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const serverURL = "http://jinrou-api.herokuapp.com"
+export const serverURL = "jinrou-app-db-do-user-13481814-0.b.db.ondigitalocean.com"
 const api = axios.create({
   // baseURL: "http://192.168.1.43:3000",
-  baseURL: "http://jinrou-api.herokuapp.com",
+  baseURL: "jinrou-app-db-do-user-13481814-0.b.db.ondigitalocean.com:25060",
 });
 
 export default api;
